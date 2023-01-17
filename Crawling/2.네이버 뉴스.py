ee29d95d1355607f48ff53c64483e40b6f3fe71a
parkgayoung96 <lunatic2563@gmail.com>
2023-01-17 17:46:13 +0900

    2023/01/17

--- Crawling/2.네이버 뉴스.py
+++ Crawling/2.네이버 뉴스.py
@@ -6,13 +6,18 @@
 
 import requests as req
 from bs4 import BeautifulSoup as bs
+from openpyxl import Workbook
+
+# 엑셀파일 생성
+workbook = Workbook()
+sheet = workbook.active
 
 pg = 1
 count = 1
 
 while True:
     # HTML 요청
-    url = 'https://news.naver.com/main/list.naver?mode=LS2D&sid2=230&sid1=105&mid=shm&date=20230116&page=%d' % pg
+    url = 'https://news.naver.com/main/list.naver?mode=LS2D&sid2=230&sid1=105&mid=shm&page=%d' % pg
     html = req.get(url, headers={'User-Agent':'Mozilla/5.0'}).text
     #print(html)
 
@@ -34,9 +39,16 @@ while True:
         title = tag_a.text
         href = tag_a['href']
 
-        print('count : ', count)
-        print('title : ', title.strip())
-        print('href : ', href.strip())
+        sheet.append([count, title.strip(), href.strip()])
+        print('%d건...' % count)
+        #print('count : ', count)
+        #print('title : ', title.strip())
+        #print('href : ', href.strip())
         count += 1
     pg += 1
-    
+
+# 엑셀 파일 저장/종료
+workbook.save('C:/Users/java2/Desktop/NaverNews.xlsx')
+workbook.close()
+
+print('프로그램 종료...')
